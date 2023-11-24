53fd9b2853d143a1aa9f7057f3247f5c704f3bf7
VitaHasten <holgerstein@hotmail.com>
2023-11-24 16:25:49 +0100

    Pilat lite med banktester och tagit bort onödiga userTests.

--- Entity.Tests/BankAccountTest.cs
+++ Entity.Tests/BankAccountTest.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Entity.Tests
+﻿namespace Entity.Tests
 {
     [TestFixture]
     public class BankAccountTests
@@ -22,11 +16,20 @@ namespace Entity.Tests
         {
             Assert.Multiple(() =>
             {
-                Assert.That(_bankAccount.AccountNumber.Equals(1234567890));
-                Assert.That(_bankAccount.AccountNumber.Equals(1234567890));
-                Assert.That(_bankAccount.AccountNumber.Equals(1234567890));
+                Assert.That(_bankAccount.AccountNumber, Is.EqualTo(1234567890));
+                Assert.That(_bankAccount.NameOfAccount, Is.EqualTo(""));
+                Assert.That(_bankAccount.Balance, Is.EqualTo(0));
+            });
 
+            Assert.Multiple(() =>
+            {
+                Assert.That(_bankAccount.Id, Is.EqualTo(0));
+                Assert.That(_bankAccount.UserAccountId, Is.EqualTo(1));
+                Assert.That(_bankAccount.Transactions, Is.Empty);
             });
         }
+
+        [Test]
+        public void TestInitialBalanceEqualToZero() => Assert.That(_bankAccount.Balance, Is.EqualTo(0));              
     }
 }
--- Entity/BankAccount.cs
+++ Entity/BankAccount.cs
@@ -9,6 +9,8 @@ namespace Entity
         public int Id { get; set; }
 
         // AccountNumber genereras automatiskt av API:t och ska vara 10 siffror.
+        [Range(1000000000, 2147483647)]
+        
         public int AccountNumber { get; private set; }
         public string NameOfAccount { get; private set; } = string.Empty;
         public decimal Balance { get; private set; }
@@ -19,12 +21,12 @@ namespace Entity
         public int UserAccountId { get; private set; }
         public virtual List<Transaction> Transactions { get; set; } = new List<Transaction>();
 
-        public BankAccount(int accountNumber, string nameOfAccount, int userAccountId, decimal initialBalance = 0)
+        public BankAccount(int accountNumber, string nameOfAccount, int userAccountId, decimal balance=0)
         {
             AccountNumber = accountNumber;
             NameOfAccount = nameOfAccount;            
             UserAccountId = userAccountId;
-            Balance = initialBalance;
+            Balance = balance;
         }
     }
 }
