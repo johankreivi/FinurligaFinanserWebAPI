5955c28ea1ed768026775ca1fe29130b99018522
Johan Kreivi <johan.kreivi@gmail.com>
2023-12-12 12:17:38 +0100

    Delete 100% code coverage

--- Infrastructure.Tests/Repositories/BankAccountRepositoryTests.cs
+++ Infrastructure.Tests/Repositories/BankAccountRepositoryTests.cs
@@ -9,6 +9,9 @@ using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
 using Microsoft.Extensions.Logging;
 using Moq;
 using Moq.EntityFrameworkCore;
+using NUnit.Framework.Internal;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System;
 
 namespace Infrastructure.Tests.Repositories
 {
@@ -34,7 +37,6 @@ namespace Infrastructure.Tests.Repositories
             _mockDataContext.Setup(x => x.UserAccounts).ReturnsDbSet(_userAccounts);
             _mockDataContext.Setup(x => x.BankAccounts).ReturnsDbSet(_bankAccounts);
             _mockUserAccountRepository = new Mock<UserAccountRepository>(_mockDataContext.Object);
-
             _sut = new BankAccountRepository(_mockDataContext.Object);
 
         }
@@ -213,19 +215,64 @@ namespace Infrastructure.Tests.Repositories
         }
 
         [Test]
-        public async Task DeleteBankAccount_WhenCalled_ReturnsBankAccount()
+        public async Task DeleteBankAccount_WhenSuccessful_ReturnsValidStatus()
+        {
+
+
+        // Arrange
+        var options = new DbContextOptionsBuilder<DataContext>()
+                .UseInMemoryDatabase(databaseName: "TestDb")
+                .Options;
+
+            var _dataContext = new DataContext(options);
+
+            var bankAccount = new BankAccount(1, "TestAccount1", 1);
+
+            _dataContext.BankAccounts.Add(bankAccount);
+            await _dataContext.SaveChangesAsync();
+
+            var sut = new BankAccountRepository(_dataContext);
+
+            // Act
+            var (resultBankAccount, resultValidationStatus) = await sut.DeleteBankAccount(1);
+
+
+            // Assert
+            Assert.That(resultBankAccount, Is.TypeOf<BankAccount>());
+            Assert.That(resultValidationStatus, Is.EqualTo(BankAccountValidationStatus.Valid));
+        }
+
+        [Test]
+        public async Task DeleteBankAccount_WhenBankAccountDoesNotExist_ReturnsNotFoundStatus()
         {
             // Arrange
-            var id = 1;
-            _mockDataContext.Setup(x => x.BankAccounts.FindAsync(id)).ReturnsAsync(_bankAccounts.Find(x => x.Id == id));
-            var result = await _sut.GetBankAccount(id);
+            var id = 10;
 
             // Act
             var (resultBankAccount, resultValidationStatus) = await _sut.DeleteBankAccount(id);
 
             // Assert
-            Assert.That(resultBankAccount, Is.TypeOf<BankAccount>());
-            Assert.That(resultValidationStatus, Is.EqualTo(BankAccountValidationStatus.Valid));
+            Assert.That(resultBankAccount, Is.Null);
+            Assert.That(resultValidationStatus, Is.EqualTo(BankAccountValidationStatus.NotFound));
+        }
+
+        [Test]
+        public async Task DeleteBankAccount_WhenBankAccountHasBalance_ReturnsInvalidAmountStatus()
+        {
+            // Arrange
+            var id = 1;
+            var bankaccount = _bankAccounts.Find(x => x.Id == id);
+            bankaccount.Balance = 100;
+
+            _mockDataContext.Setup(x => x.BankAccounts.FindAsync(id)).ReturnsAsync(bankaccount);
+
+
+            // Act
+            var (resultBankAccount, resultValidationStatus) = await _sut.DeleteBankAccount(id);
+
+            // Assert
+            Assert.That(resultBankAccount, Is.Null);
+            Assert.That(resultValidationStatus, Is.EqualTo(BankAccountValidationStatus.Invalid_amount));
         }
 
     }
